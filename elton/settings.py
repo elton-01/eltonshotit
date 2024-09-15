bef6a943933a7d8d6682c07e22131ea021e3e555
oncampus.teamgh@gmail.com <oncampus.teamgh@gmail.com>
2024-09-15 14:51:24 +0000

    Deploying to github

--- elton/settings.py
+++ elton/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-2m)ewm$6k@@dxrjbm=(_6v8fbo(8-%*bsf-h%n_p3qctaz(-ms"
+SECRET_KEY = "django-insecure-2m)ewm$6k@@dxrjbm=(_6v8fbo(8-*bsf-h%n_p3qctaz(-ms"
+
+# SECRET_KEY = "django-insecure-2m)ewm$6k@@dxrjbm=(_6v8fbo(8-%*bsf-h%n_p3qctaz(-ms"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -129,6 +131,7 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 MEDIA_ROOT = os.path.join(BASE_DIR, "static/assets/img/portfolio-admin/")
+MEDIA_URL = '/assets/img/portfolio-admin/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
