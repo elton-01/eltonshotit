215de56f57be02943e91a193983c6424fc4308bb
oncampus.teamgh@gmail.com <oncampus.teamgh@gmail.com>
2024-09-16 15:06:42 +0000

    Deploying to github

--- eltonapp/views.py
+++ eltonapp/views.py
@@ -2,9 +2,24 @@ from django.shortcuts import render
 from .models import *
 from django.urls import path
 
+
 # Create your views here.
 def index(request):
     imageGroup = ImageGroup.objects.all()
     images = Image.objects.all()
-    context = {"images":images, "group":imageGroup}
-    return render(request, 'index.html', context)
+    context = {
+        "images": images,
+        "group": imageGroup,
+    }
+    return render(
+        request,
+        "index.html",
+        context,
+    )
+
+
+def form(request):
+    return render(
+        request,
+        "form.html",
+    )
