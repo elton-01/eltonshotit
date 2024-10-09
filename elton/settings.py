e8b022eae516ab9d34440f3242bc08fe7a253c5a
oncampus.teamgh@gmail.com <oncampus.teamgh@gmail.com>
2024-10-09 03:36:38 +0000

    Deploying to github

--- elton/settings.py
+++ elton/settings.py
@@ -157,9 +157,6 @@ cloudinary.config(
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
